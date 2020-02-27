53a24c7923216b3f1dea2ede5d0e71bdc01b9777
Franky Mai <frankymai91@gmail.com>
2020-02-27 23:03:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
-def introduction_with_language(name, language)
+def introduction_with_language(name,language)
   
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
 
