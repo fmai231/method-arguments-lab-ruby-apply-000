cbae3b66f84af6b1dbc356716826c3e009348a02
Franky Mai <frankymai91@gmail.com>
2020-02-27 23:04:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,11 @@
-def introduction(name,language)
+def introduction(name)
   
-puts "Hi, my name is #{name} and I am learning to program in #{language}."
+puts "Hi, my name is #{name}."
   
 end
 
 
-
+introduction(jos)
 # describe "#introduction" do 
 #   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
 #     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
