ef45e46907a2a890ef265f9fa34a838b97e0509b
Franky Mai <frankymai91@gmail.com>
2020-02-27 23:06:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,8 @@ puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name,language)
-  
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 # describe "#introduction" do 
 #   it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
